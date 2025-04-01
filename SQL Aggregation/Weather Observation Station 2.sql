714132c1a276f375c0fa0687dba406d664766af3
TuanLC <luucungtuan2907@gmail.com>
2025-04-01 23:53:10 +0700

    [20250401] implement SQL Aggregation.

--- SQL Aggregation/Weather Observation Station 2.sql
+++ SQL Aggregation/Weather Observation Station 2.sql
@@ -2,3 +2,5 @@
 
 -- The sum of all values in LAT_N rounded to a scale of 2 decimal places.
 -- The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2)
+FROM STATION;
